32490404a6d879f06f8ee7d7de2e8d129108a665
Thosedarnmonkeys <thosedarnmonkeys@googlemail.com>
2016-03-15 17:08:03 +0000

    Moved exception handling to event handlers, ALSO MORE FORMATTING

--- SurfacePenOnlyMode/PenOnlyTrayApp.cs
+++ SurfacePenOnlyMode/PenOnlyTrayApp.cs
@@ -9,117 +9,126 @@ using System.ComponentModel;
 
 namespace SurfacePenOnlyMode
 {
-    public class PenOnlyTrayApp : Form
+  public class PenOnlyTrayApp : Form
+  {
+    #region private fields
+    private ContextMenu trayMenu;
+    private NotifyIcon trayIcon;
+    private readonly Func<string, bool> matchFunc = s => s.ToLower() == "hid-compliant touch screen";
+    #endregion
+
+    #region public methods
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    public static void Main()
     {
-        private ContextMenu trayMenu;
-        private NotifyIcon trayIcon;
-        private Func<string, bool> matchFunc = s => s.ToLower() == "hid-compliant touch screen";
-
-            /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        public static void Main()
-        {
-            Application.Run(new PenOnlyTrayApp());
-        }
-
-        public PenOnlyTrayApp()
-        {
-            trayMenu = new ContextMenu();
-
-            trayMenu.MenuItems.Add("Enable Pen Only Mode", OnClickEnablePenOnlyMode);
-            trayMenu.MenuItems.Add("-"); //Creates a seperator
-            trayMenu.MenuItems.Add("Exit", OnClickExit);
-
-
-            trayIcon = new NotifyIcon();
-            trayIcon.Text = "MyTrayApp";
-            trayIcon.Icon = new Icon(SystemIcons.Asterisk, 40, 40);
-
-            trayIcon.ContextMenu = trayMenu;
-            trayIcon.Visible = true;
-
-            //set driver enabled under any kind of power change
-            SystemEvents.SessionEnding += OnDeviceStateChange; 
-            SystemEvents.PowerModeChanged += OnDeviceStateChange;
-        }
-
-        private void OnClickDisablePenOnlyMode(object sender, EventArgs e)
-        {
-            SetDriverOn();
-
-            trayMenu.MenuItems[0].Click -= OnClickDisablePenOnlyMode;
-            trayMenu.MenuItems[0].Click += OnClickEnablePenOnlyMode;
-            trayMenu.MenuItems[0].Text = "Enable Pen Only Mode";
-        }
-
-        private void OnClickEnablePenOnlyMode(object sender, EventArgs e)
-        {
-            SetDriverOff();
-
-            trayMenu.MenuItems[0].Click -= OnClickEnablePenOnlyMode;
-            trayMenu.MenuItems[0].Click += OnClickDisablePenOnlyMode;
-            trayMenu.MenuItems[0].Text = "Disable Pen Only Mode";
-        }
-
-        private void OnDeviceStateChange(object sender, EventArgs e)
-        {
-            SetDriverOn();
-        }
-
-        private void SetDriverOn()
-        {
-            try
-            {
-                HardwareManager.SetDeviceState(matchFunc, false);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show("Error while trying to set driver on: " + e.Message);
-            }
-        }
-
-        private void SetDriverOff()
-        {
-            try
-            {
-                HardwareManager.SetDeviceState(matchFunc, true);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show("Error while trying to set driver off: " + e.Message);
-            }
-        }
-
-        private void OnClickExit(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        protected override void OnLoad(EventArgs e)
-        {
-            Visible = false;
-            ShowInTaskbar = false;
-
-            base.OnLoad(e);
-        }
-
-        protected override void OnClosing(CancelEventArgs e)
-        {
-            trayIcon = null;
-
-            OnDeviceStateChange(this, new EventArgs());
-
-            base.OnClosing(e);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            SystemEvents.PowerModeChanged -= OnDeviceStateChange;
-            SystemEvents.SessionEnding -= OnDeviceStateChange;
-
-            base.Dispose(disposing);
-        }
+      Application.Run(new PenOnlyTrayApp());
     }
+
+    public PenOnlyTrayApp()
+    {
+      trayMenu = new ContextMenu();
+
+      trayMenu.MenuItems.Add("Enable Pen Only Mode", OnClickEnablePenOnlyMode);
+      trayMenu.MenuItems.Add("-"); //Creates a seperator
+      trayMenu.MenuItems.Add("Exit", OnClickExit);
+
+
+      trayIcon = new NotifyIcon();
+      trayIcon.Text = "MyTrayApp";
+      trayIcon.Icon = new Icon(SystemIcons.Asterisk, 40, 40);
+
+      trayIcon.ContextMenu = trayMenu;
+      trayIcon.Visible = true;
+
+      //set driver enabled under any kind of power change
+      SystemEvents.SessionEnding += OnDeviceStateChange;
+      SystemEvents.PowerModeChanged += OnDeviceStateChange;
+    }
+    #endregion
+
+    #region private event handlers
+    private void OnClickDisablePenOnlyMode(object sender, EventArgs e)
+    {
+      try
+      {
+        SetDriverOn();
+
+        trayMenu.MenuItems[0].Click -= OnClickDisablePenOnlyMode;
+        trayMenu.MenuItems[0].Click += OnClickEnablePenOnlyMode;
+        trayMenu.MenuItems[0].Text = "Enable Pen Only Mode";
+      }
+      catch (Exception except)
+      {
+        MessageBox.Show("Error while trying to set driver on: " + except.Message);
+      }
+    }
+
+    private void OnClickEnablePenOnlyMode(object sender, EventArgs e)
+    {
+      try
+      {
+        SetDriverOff();
+
+        trayMenu.MenuItems[0].Click -= OnClickEnablePenOnlyMode;
+        trayMenu.MenuItems[0].Click += OnClickDisablePenOnlyMode;
+        trayMenu.MenuItems[0].Text = "Disable Pen Only Mode";
+      }
+      catch (Exception except)
+      {
+        MessageBox.Show("Error while trying to set driver off: " + except.Message);
+      }
+    }
+
+    private void OnDeviceStateChange(object sender, EventArgs e)
+    {
+      SetDriverOn();
+    }
+    private void OnClickExit(object sender, EventArgs e)
+    {
+      Application.Exit();
+    }
+    #endregion
+
+    #region private methods
+    private void SetDriverOn()
+    {
+      HardwareManager.SetDeviceState(matchFunc, false);
+    }
+
+    private void SetDriverOff()
+    {
+      HardwareManager.SetDeviceState(matchFunc, true);
+    }
+    #endregion
+
+    #region overidden methods
+    protected override void OnLoad(EventArgs e)
+    {
+      Visible = false;
+      ShowInTaskbar = false;
+
+      base.OnLoad(e);
+    }
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+      trayIcon = null;
+
+      OnDeviceStateChange(this, new EventArgs());
+
+      base.OnClosing(e);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      SystemEvents.PowerModeChanged -= OnDeviceStateChange;
+      SystemEvents.SessionEnding -= OnDeviceStateChange;
+
+      base.Dispose(disposing);
+    }
+    #endregion
+  }
 }
